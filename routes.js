b0e12fb4d0cdbad356075b2083c2c27cfb0fbe7f
Ccino Lee <ccinodev@gmail.com>
2020-03-22 17:52:33 +0900

    #3.12 Searching Videos

--- routes.js
+++ routes.js
@@ -11,7 +11,6 @@ const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
-
 // Videos
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
@@ -19,36 +18,45 @@ const VIDEOS_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
-
 const routes = {
-    home: HOME,
-    join: JOIN,
-    login: LOGIN,
-    logout: LOGOUT,
-    search: SEARCH,
-    users: USERS,
-    userDetail: (id) => {
-        if (id) {
-            return `/users/${id}`;
-        }
-        else {
-            return USER_DETAIL;
-        }
-    },
-    editProfile: EDIT_PROFILE,
-    changePassword: CHANGE_PASSWORD,
-    videos: VIDEOS,
-    upload: UPLOAD,
-    videoDetail: (id) => {
-        if (id) {
-            return `/videos/${id}`;
-        }
-        else {
-            return VIDEOS_DETAIL;
-        }
-    },
-    editVideo: EDIT_VIDEO,
-    deleteVideo: DELETE_VIDEO
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: id => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
+    }
+  },
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: id => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEOS_DETAIL;
+    }
+  },
+  editVideo: id => {
+    if (id) {
+      return `/videos/${id}/edit`;
+    } else {
+      return EDIT_VIDEO;
+    }
+  },
+  deleteVideo: id => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    } else {
+      return DELETE_VIDEO;
+    }
+  }
 };
 
 export default routes;
