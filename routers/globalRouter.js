8eb1fa8091319b745e6516801e52c7fd29d63017
Ccino Lee <ccinodev@gmail.com>
2020-03-26 12:02:08 +0900

    #6.9 Recap and User Profile

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,18 +1,40 @@
 import express from "express";
 import routes from "../routes";
+import passport from "passport";
 import { home, search } from "../controllers/videoController";
-import { logout, getJoin, postJoin, getLogin, postLogin } from "../controllers/userController";
+import {
+  logout,
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+  githubLogin,
+  postGithubLogIn,
+  getMe
+} from "../controllers/userController";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+globalRouter.get(routes.github, githubLogin);
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate("github", {
+    failureRedirect: "/login"
+  }),
+  postGithubLogIn
+);
+
+globalRouter.get(routes.me, getMe);
 
 export default globalRouter;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,16 @@
 import express from "express";
 import routes from "../routes";
-import { userDetail, editProfile, changePassword } from "../controllers/userController";
+import {
+  userDetail,
+  editProfile,
+  changePassword
+} from "../controllers/userController";
+import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.userDetail(), userDetail);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.userDetail(), onlyPrivate, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
-
-
