69ffff7d68456bdda4395284f7f3e5656650068d
Ccino Lee <ccinodev@gmail.com>
2020-04-04 17:41:59 +0900

    #7.3 Adding Creator to Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -33,16 +33,14 @@ export const postUpload = async (req, res) => {
     body: { title, description },
     file: { path }
   } = req;
-
-  // res.render("upload", { pageTitle: "Upload" });
-
   const newVideo = await Video.create({
     fileUrl: path,
     title,
-    description
+    description,
+    creator: req.user.id
   });
-  console.log(newVideo);
-
+  req.user.videos.push(newVideo.id);
+  req.user.save();
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -51,7 +49,8 @@ export const videoDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const video = await Video.findById(id);
+    const video = await Video.findById(id).populate("creator");
+    console.log(video.title);
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
--- models/Comment.js
+++ models/Comment.js
@@ -1,15 +1,19 @@
-import mongoos from "mongoose";
+import mongoose from "mongoose";
 
-const CommentSchema = new mongoos.Schema({
-    text: {
-        type: String,
-        required: "Text is required"
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
+const CommentSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: "Text is required"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
 });
 
-const model = mongoos.model("Comment", CommentSchema);
+const model = mongoose.model("Comment", CommentSchema);
 export default model;
